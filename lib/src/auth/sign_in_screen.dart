67f3a8c0403f1baa9f6f955ad5c896eff3b3d1ab
MathSena <MathSena07@hotmail.com>
2022-11-24 13:26:09 -0300

    Tela de Cadastro

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:mais_verde/src/auth/components/custom_text_field.dart';
+import 'package:mais_verde/src/auth/sign_up_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -127,7 +128,15 @@ class SignInScreen extends StatelessWidget {
                               color: Colors.green,
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (c){
+                                return SignUpScreen();
+                              }
+                              ),
+                            );
+                          },
                           child: const Text('Criar conta',
                               style: TextStyle(
                                 fontSize: 18,
