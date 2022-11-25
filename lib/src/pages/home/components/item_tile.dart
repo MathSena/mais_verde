13729d8b6fb50d136815473233f71fe7186bd781
MathSena <MathSena07@hotmail.com>
2022-11-24 23:06:45 -0300

    Tela detalhes de produto

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../models/item_model.dart';
 import '../../../services/utils_services.dart';
+import '../../product/product_screen.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
@@ -14,44 +15,54 @@ class ItemTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Card(
-            elevation: 1,
-            shadowColor: Colors.grey.shade300,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Image
-                  Expanded(child: Image.asset(item.imgUrl)),
-                  //Nome
-                  Text(item.itemName,
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold)),
-                  //Preço
-                  Row(
-                    children: [
-                      Text(utilsServices.priceToCurrency(item.price),
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.lightGreen)),
-                      Text(
-                        '/${item.unit}',
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ),
-                      )
-                    ],
-                  )
-                ],
+
+        GestureDetector(
+          onTap:(){
+            Navigator.of(context).push(MaterialPageRoute(builder: (c){
+              return ProductScreen(item: item);
+            }));
+    },
+          child: Card(
+              elevation: 1,
+              shadowColor: Colors.grey.shade300,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
               ),
-            )),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Image
+                    Expanded(child: Hero(
+                        tag: item.imgUrl,
+                        child: Image.asset(item.imgUrl))),
+                    //Nome
+                    Text(item.itemName,
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    //Preço
+                    Row(
+                      children: [
+                        Text(utilsServices.priceToCurrency(item.price),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.lightGreen)),
+                        Text(
+                          '/${item.unit}',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ),
         Positioned(
             top: 4,
             right: 4,
